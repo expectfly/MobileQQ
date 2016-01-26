c531c9e4fd45bd424a636d215f3bb0a25e940c5a
安第斯神鹰 <wall-e@live.cn>
2016-01-26 11:25:31 +0800

    上传 VS 示例

---
+++ app/pages/login/login.js
@@ -0,0 +1,31 @@
+import {IonicApp, Page, NavController} from 'ionic/ionic';
+import {TabsPage} from '../tabs/tabs';
+import {SignupPage} from '../signup/signup';
+import {UserData} from '../../providers/user-data';
+
+
+@Page({
+  templateUrl: 'build/pages/login/login.html'
+})
+export class LoginPage {
+  constructor(nav: NavController, userData: UserData) {
+    this.nav = nav;
+    this.userData = userData;
+
+    this.login = {};
+    this.submitted = false;
+  }
+
+  onLogin(form) {
+    this.submitted = true;
+
+    if (form.valid) {
+      this.userData.login();
+      this.nav.push(TabsPage);
+    }
+  }
+
+  onSignup() {
+    this.nav.push(SignupPage);
+  }
+}
